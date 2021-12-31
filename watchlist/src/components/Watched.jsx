99a663473acdaa92a98e5d45ecf8382772bcff98
malikareej <areejmalik2699@gmail.com>
2021-12-31 15:08:33 +0500

    Watchlist

---
+++ watchlist/src/components/Watched.jsx
@@ -0,0 +1,24 @@
+import React, {useContext} from 'react';
+import { GlobalContext } from '../context/GlobalState';
+import { MovieCard } from './MovieCard';
+
+export const Watched = () => {
+    const {watched}= useContext(GlobalContext);
+    return (
+        <div className="movie-page">
+            <div className="container">
+                <div className="header">
+                    <h1 className="heading">Watched Movies</h1>
+                </div>
+                {watched.length>0 ?(<div className="movie-grid">
+                    {watched.map((movie) =>(
+                    <MovieCard movie={movie} type="watched"/>
+                    ))}
+                </div>): (
+                    <h2 className="no-movies">No Watched Movie</h2>
+                )}
+                
+            </div>
+        </div>
+    )
+}
